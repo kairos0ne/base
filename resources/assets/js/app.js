0b6d7761993731f40b939912c121ebee77d759b1
kairos0ne <sdpersonal.sd@gmail.com>
2016-05-30 17:23:14 +0100

    remove unused directives

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -11,85 +11,10 @@ Vue.config.debug = true;
 Vue.http.headers.common['X-CSRF-TOKEN'] = document.querySelector('#csrf-token').getAttribute('content');
 
 // Directives
-// Amazon S3 Directive - directive to save files requires additional work and dropzone intergration.
-Vue.directive('savefile', {
-   params: ['S3'],
-    bind: function () {
-        this.el.addEventListener(
-            'submit', this.onSubmit.bind(this)
-            );  
-    },
 
-    onSubmit: function (e) {
-        var requestType = this.getRequestType();
-        this.vm
-            .$http[requestType](this.el.action)
-            .then(this.onS3.bind(this))
-            .catch(this.onError.bind(this));
-        e.preventDefault();
-    },
-    onS3: function(){
-        if (this.params.s3) {
-            alert(this.params.complete); // Use pretty flash message instead.
-        }
-    },
+// Note include aws save to s3 directive at a later date
 
-    onError: function (response) {
-        alert(response.data.message); // Use pretty flash message instead.
-    },
-
-    //get the request type from the mthod
-    getRequestType: function () {
-        var method = this.el.querySelector('input[name="_method"]');
-        return (method ? method.value : this.el.method).toLowerCase();
-    }
-});
-
-// Custom directive to store forms 
-Vue.directive('ajax', {
-    params: ['complete'],
-
-    bind: function () {
-        this.el.addEventListener(
-            'submit', this.onSubmit.bind(this)
-        );
-    },
-
-    onSubmit: function (e) {
-        var postData = this.request.formData();
-        var requestType = this.getRequestType();
-
-        this.vm
-            .$http[requestType](this.el.action)
-            .then(this.onComplete.bind(this))
-            .catch(this.onError.bind(this));
-
-        e.preventDefault();
-    },
-
-    onComplete: function () {
-        if (this.params.complete) {
-            alert(this.params.complete); // Use pretty flash message instead.
-        }
-    },
-
-    onError: function (response) {
-        alert(response.data.message); // Use pretty flash message instead.
-    },
-
-    //get the request type from the method
-    getRequestType: function () {
-        var method = this.el.querySelector('input[name="_method"]');
-        return (method ? method.value : this.el.method).toLowerCase();
-    },
-    formData: function (e) {
-        e.preventDefault();
-       //get form data as array 
-    }
-});
-
-
-//Import Components for the Application, these are the componants that belong 
+// Import Components for the Application, these are the componants that belong 
 // to the main vue instance and is Global for the site and bound to the vueapplication div
 
 // Portfolio - resources/assets/js/components/portfolio
@@ -122,8 +47,8 @@ new Vue({
     el: '#vueapplication',
     http: {
         headers: {
-            // You could also store your token in a global object,
-            // and reference it here. APP.token
+            // Token is currently stored in a global variable 
+            // Use the below method if you'd like to change this to be parsed on the form
             // 'X-CSRF-TOKEN': document.querySelector('input[name="_token"]').value
         }
     },
