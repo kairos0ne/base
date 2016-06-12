ca282052c60dd51203af73c68ab9853f20bee6f3
kairos0ne <sdpersonal.sd@gmail.com>
2016-06-12 13:26:30 +0100

    Include welcome text in intro vue

--- resources/assets/js/components/page_assets/introduction.vue
+++ resources/assets/js/components/page_assets/introduction.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <h2 class="survey col-lg-offset-2 col-lg-8 col-md-8 col-sm-12">Welcome {{ username }}. I see this is your first time on the site. I hope the next couple of steps help you define a strategic approach to developing online tools.</h2>
+        <h2 class="survey col-lg-offset-2 col-lg-8 col-md-8 col-sm-12">Welcome {{ username }}. I see this is your first time on the site. I hope the next couple of steps help you define a strategic approach to developing online tools. Before we get started there are a few details we'll need to setup. It won't take a minute, follow the steps.</h2>
     </div>
 </template>
 <script>
